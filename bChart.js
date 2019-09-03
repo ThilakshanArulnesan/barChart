2b4c2cf4697eca355f7da7b5e45836d0e50e0426
Thilakshan Arulnesan <th_thilakshan@hotmail.com>
2019-09-03 19:25:26 -0400

    added left axis and bars that scale properly. Used dividers and CSS styling to ensure bars were proper sizes.

--- bChart.js
+++ bChart.js
@@ -1,10 +1,57 @@
-function drawBarChart(data, options, element){
-    document.getElementById("demo").innerHTML = Date();
-    element.text("test");
-    console.log(element);
+function drawBarChart(data, options, element) {
+
+
+    if (options.width === undefined) options.width = 500;
+    if (options.height === undefined) options.height = 500;
+
+
+    drawAxes(options.height, options.width, element);
+    drawBars(data, element, options.height, options.width);
+
+    let bgColor = "red";
+
+    console.log(options);
 }
 
-function test(){
-    $("h1").text("CHANGED");
+function drawAxes(h, w, e) {
+    //creates and adds a divider with the height + width in element e.
+    let st = '<div style="width:' + 10 + "px" + ';height:' + h + 'px;float:left;background-color: black"></div>';
 
+    e.append(st);
+    //  st = '<div style="width:' + w + "px" + ';height:' + 10 + 'px;background-color: black"></div>';
+    //  e.append(st);
+}
+
+function drawBars(data, e, h, w) {
+    let maxVal = findMax(data);
+    let nBars = data.length;
+    console.log(maxVal);
+    for (barVal of data) {
+        console.log(barVal);
+        let st = '<div style="width:' + w / nBars + "px" + ';height:' + h + 'px;float:left">';
+        st = st + '<div style="width:' + 100 + "%" +
+            ';height:' + h * (1 - barVal / maxVal) +
+            'px;float:left"></div>';
+
+        st = st + '<div style="width:' + 100 + "%" +
+            ';height:' + h * (barVal / maxVal) +
+            'px;float:left;background-color: green"></div></div>';
+        console.log(st);
+        e.append(st);
+    }
+
+
+}
+
+
+function findMax(d) {
+    let max = 0;
+    for (datum of d) {
+        if (datum > max) max = datum;
+    }
+    return max;
+}
+
+function test() {
+    $("h1").text("CHANGED");
 }
