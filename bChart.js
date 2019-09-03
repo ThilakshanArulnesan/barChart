ccde031f70d2f25123f2f3b1d71c9dc3db5924be
Thilakshan Arulnesan <th_thilakshan@hotmail.com>
2019-09-03 17:54:45 -0400

    Added html page, scripts, filestructure

---
+++ bChart.js
@@ -0,0 +1,10 @@
+function drawBarChart(data, options, element){
+    document.getElementById("demo").innerHTML = Date();
+    element.text("test");
+    console.log(element);
+}
+
+function test(){
+    $("h1").text("CHANGED");
+
+}
