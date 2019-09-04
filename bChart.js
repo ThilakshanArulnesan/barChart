496ce4bcac1c20cea7b76f6dd11d704b32845dce
Thilakshan Arulnesan <th_thilakshan@hotmail.com>
2019-09-04 18:54:05 -0400

    Added dynamic label position options, label colors (restructured data from array to an object to have custom labels). Also added a title that resizes the chart along with color and size options for the title. Finally spacing was added to the bars and a custom formula was implemented to resize the width of the bars as needed.

--- bChart.js
+++ bChart.js
@@ -1,21 +1,22 @@
 function drawBarChart(data, options, element) {
-
+    const AXES_WIDTH = 10;
     //Sets default properties if they are undefined
     options = setDefaults(options);
 
     let appendText = "";
     let blnTitle = options.titleText === undefined;
 
-    appendText += '<div class="barchart" style="position:relative">'; //Container for barchart
+    appendText += '<div class="barchart" >'; //Container for barchart
 
     if (!blnTitle) {
         appendText += drawDiv(options.titleSize, options.width, options.titleText, "title"); //Title
-        options.height = options.height - options.titleSize; //removes the space used for the title    
     }
-    appendText += drawDiv(options.height, 10, "", "axis"); //Y-axis   
-    appendText += drawBars(data, element, options.height, options.width, options.color);
-    appendText += drawDiv(10, options.width, "", "axis"); //X -axis
 
+    appendText += drawDiv(options.height - options.titleSize, AXES_WIDTH, "", "axis"); //Y-axis   
+
+    //Subtract 10 from the width of the bars because there is less space due to axes
+    appendText += drawBars(data, options.height- options.titleSize, options.width-AXES_WIDTH, options.spacing);
+    appendText += drawDiv(10, options.width, "", "axis"); //X -axis
 
     appendText += '</div>';
     element.append(appendText);
@@ -24,13 +25,34 @@ function drawBarChart(data, options, element) {
     $(".barchart").css({ "width": options.width });
     $(".bar").css({ "backgroundColor": options.color });
     $(".axis").css({ "backgroundColor": "black" });
-
-
     $(".title").css({ "color": options.titleColor });
     $(".title").css({ "font-size": options.titleSize + "px" });
 
+    formatLabel(options.labelCentering,options.labelColor);
+}
 
+function formatLabel(cent,col) {
+    switch (cent) {
+        case "top":
+            cent = 0;
+            break;
+        case "middle":
+            cent = 50;
+            break;
+        case "bottom":
+            cent = 70;
+            break;
+    }
 
+    if (isNaN(cent)) {
+        cent = 50;
+    }
+
+    $(".label").css({
+         "top": cent + "%",
+         "color": col
+ });
+    
 }
 
 function setDefaults(options) {
@@ -38,9 +60,11 @@ function setDefaults(options) {
     if (options.width === undefined) options.width = 500;
     if (options.height === undefined) options.height = 500;
     if (options.color === undefined) options.color = "black";
-    if (options.labelColor === undefined) options.labelColor = "black";
+    //if (options.labelColor === undefined) options.labelColor = "black";
     if (options.titleColor === undefined) options.titleColor = "black";
-    if (options.titleSize === undefined) options.titleSize = "12px";
+    if (options.titleSize === undefined) options.titleSize = "12";
+    if (options.spacing === undefined) options.spacing = "0";
+    if (options.labelCentering === undefined) options.labelCentering = "50";
 
     return options;
 
@@ -57,39 +81,38 @@ function drawDiv(h, w, text, className) {
 }
 
 
-function drawDiv2(h, w, text) {
-    //creates and adds a divider with the height + width in element e.
-    let st = '<div style="width:' + w + "px" + ';height:' + h + 'px;float:left;background-color: black">' + text + '</div>';
-    return st;
-}
-/* Remove this function if not required later
-function drawXAxes(h, w,e) {
-    //creates and adds a divider with the height + width in element e.
-    let st = '<div style="width:' + w + "px" + ';height:' + h + 'px;position:absolute;bottom:0;background-color: black"></div>';
-    return st;
-}
-*/
-
-function drawBars(data, e, h, w, bufferWidth) {
+function drawBars(data, h, w, bufferWidth) {
     let maxVal = findMax(data); //Finds the maximum value, normalizes the sizes of the bars using this value
-    let nBars = data.length; //# of bars of data we have to add
+    let nBars = Object.keys(data).length; //# of bars of data we have to add
     let st = "";
+
+    //Between each bar there is a 'blank space' of width bufferWidth
+    //So the width of the bars is the total width minus the space taken by the blank bars
+    //If there are nBars bars, then there are nbars+1 buffers (one at beginning, one at the end)
+    //So the width of the bar is the remaining space divided by the numebr of bars 
+    let barWidth = (w - bufferWidth * (nBars+1)) / (nBars );
+
     console.log(maxVal);
-    for (barVal of data) {
-        console.log(barVal);
+    st += '<div style="width:' + bufferWidth + "px" + ';height:' + h + 'px;float:left"></div>';
+
+    for (barVal in data) {
+        console.log(data[barVal]);
 
         //Adds a bar (containing data):
-        st += '<div style="width:' + w / (nBars + 1) + "px" + ';height:' + h + 'px;float:left">';
+
+        st += '<div style="width:' + barWidth + "px" + ';height:' + h + 'px;float:left">';
         //Top part of the bar (the part that is not filled in)
         st += '<div style="width:' + 100 + "%" +
-            ';height:' + h * (1 - barVal / maxVal) +
+            ';height:' + h * (1 - data[barVal] / maxVal) +
             'px;float:left"></div>';
         //Bottom half of the bar (part that is filled in with data)
         st += '<div class="bar" style="width:' + 100 + "%" +
-            ';height:' + h * (barVal / maxVal) +
+            ';height:' + h * (data[barVal] / maxVal) +
             'px;float:left;vertical-align:middle">' +
-            '<span class="label vertical-center">' + barVal +
+            '<span class="label">' + data[barVal] +
             '</span></div></div>';
+        st += '<div style="width:' + bufferWidth + "px" + ';height:' + h + 'px;float:left"></div>';
+
 
 
     }
@@ -100,8 +123,8 @@ function drawBars(data, e, h, w, bufferWidth) {
 
 function findMax(d) {
     let max = 0;
-    for (datum of d) {
-        if (datum > max) max = datum;
+    for (datum in d) {
+        if (d[datum] > max) max = d[datum];
     }
     return max;
 }
