b6d3ef398818ec2cf088f4da2cff74f68b903bae
Thilakshan Arulnesan <th_thilakshan@hotmail.com>
2019-09-06 17:31:10 -0400

    Added some coments, reorganized functions

--- bChart.js
+++ bChart.js
@@ -42,82 +42,6 @@ function drawTitle(h, w, text, col) {
 
 
 
-function drawTicks(h, w, d, yMax, nTicks) {
-  const SIZE_OF_TICKS = 3;
-  let sizeOfBlanks = (h - nTicks * SIZE_OF_TICKS) / nTicks;
-  console.log(yMax);
-
-  if (yMax === undefined || yMax < findMax(d)) {
-    yMax = findMax(d);
-  }
-
-  //creates a divider to put all the ticks in of height h, width w
-  let st = '<div class="yticks" style="width:' + w + "px" +
-    ';height:' + h +
-    'px;float:left;text-align:center">';
-
-  for (let i = 0; i < nTicks; i++) {
-
-    st += '<div  class="tick" style="width:' + w / 3 + "px" +
-      ';height:' + SIZE_OF_TICKS +
-      'px;float:right;background-color:black">' +
-      '</div>';
-
-    st += '<div  class="tick" style="width:' + 2 * w / 3 + "px" +
-      ';height:' + SIZE_OF_TICKS +
-      'px;float:right">' +
-      ((nTicks - i) * yMax / nTicks).toFixed(1) + '</div>';
-
-    st += '<div  class="blankSpace" style="width:' + w + "px" +
-      ';height:' + sizeOfBlanks +
-      'px;float:left;text-align:center"></div>';
-
-  }
-  return st + '</div>';
-}
-
-function arrayify(d) {
-  //Changes any values that are not an array into an array of length one.
-  //This is in order to ensure the rest of the functions can assume an array is given.
-  for (key of Object.keys(d)) {
-    let val = d[key];
-    if (typeof val != "object") {
-      d[key] = [val];
-    }
-  }
-  return d;
-}
-
-function formatLabel(cent, col, e) {
-  console.log(cent);
-  //Changes CSS style of label depending on where the user would like the label to be located
-  switch (cent) {
-    case "top": //label at the top
-      e.find(".label").css({
-        "top": "3px"
-      });
-      break;
-    case "center": //label in the center
-      e.find(".label").css({
-        "top": 50 + "%"
-      });
-      break;
-    case "bottom": //label at the bottom
-      e.find(".label").css({
-        "bottom": "3px"
-      });
-
-      break;
-    default: //If not provided, defautls to in the middle
-      e.find(".label").css({
-        "top": 50 + "%",
-        "color": col
-      });
-  }
-
-
-
-}
 
 function setDefaults(options) {
   //This functions sets default parameters if they are not defined.
@@ -132,6 +56,7 @@ function setDefaults(options) {
     options.labelColor = [c];
   }
 
+  //Default values for parameters if none are provided by the user.
   if (options.width === undefined) options.width = 500;
   if (options.height === undefined) options.height = 500;
   if (options.color === undefined) options.color = ["#7293cb", "#e1974c", "#84ba5b", "#d35e60"];
@@ -145,41 +70,6 @@ function setDefaults(options) {
 
 }
 
-function drawDiv(h, w, text, className) {
-  //creates and adds a divider with the height + width in element e.
-  let st = '<div id="title" class="' +
-    className +
-    '" style="width:' + w + "px" +
-    ';height:' + h +
-    'px;float:left;text-align:center">' + text + '</div>';
-  return st;
-}
-
-
-function drawXLabels(data, h, w, bufferWidth) {
-  let nBars = Object.keys(data).length; //# of labels of data we have to add
-  let st = "";
-
-  //Between each label there is a 'blank space' of width bufferWidth
-  //So the width of the labels is the total width minus the space taken by the blank bars
-  //If there are nBars labels, then there are nbars+1 label buffers (one at beginning, one at the end)
-  //So the width of the label is the remaining space divided by the number of bars
-  let barWidth = (w - bufferWidth * (nBars + 1)) / (nBars);
-
-  //This adds a space between the y-axis and the first bar.
-  let initialSpace = parseFloat(bufferWidth) + 60.;
-  st += '<div style="width:' + initialSpace + "px" + ';height:' + h + 'px;float:left"></div>';
-
-  for (barVals in data) {
-    //Creates a label divider. The class xlabel can be manipulated as needed
-    st += '<div class = "xlabel" style="width:' + barWidth + "px" + ';height:' + h + 'px"> '
-      + barVals + '</div>';
-
-    //This adds a space after each bar
-    st += '<div style="width:' + bufferWidth + "px" + ';height:' + h + 'px;float:left"></div>';
-  }
-  return st;
-}
 
 
 function drawBars(data, maxVal, h, w, bufferWidth, colors, labelColors) {
@@ -236,6 +126,129 @@ function drawBars(data, maxVal, h, w, bufferWidth, colors, labelColors) {
 }
 
 
+
+function formatLabel(cent, col, e) {
+
+  //Changes CSS style of label depending on where the user would like the label to be located
+  switch (cent) {
+    case "top": //label at the top
+      e.find(".label").css({
+        "top": "3px"
+      });
+      break;
+    case "center": //label in the center
+      e.find(".label").css({
+        "top": 50 + "%"
+      });
+      break;
+    case "bottom": //label at the bottom
+      e.find(".label").css({
+        "bottom": "3px"
+      });
+
+      break;
+    default: //If not provided, defautls to in the middle
+      e.find(".label").css({
+        "top": 50 + "%",
+        "color": col
+      });
+  }
+
+
+
+}
+
+
+function drawTicks(h, w, d, yMax, nTicks) {
+  // This function adds ticks to the y-axis based on the max value of the data or the max value provided by the user
+  const SIZE_OF_TICKS = 3; 
+  let sizeOfBlanks = (h - nTicks * SIZE_OF_TICKS) / nTicks; //This is the empty space in between the ticks.
+
+  if (yMax === undefined || yMax < findMax(d)) { //If the user has not specified a maxvalue OR the user's max value is too low, use the max value of the data
+    yMax = findMax(d);
+  }
+
+  //creates a divider to put all the ticks in of height h, width w
+  let st = '<div class="yticks" style="width:' + w + "px" +
+    ';height:' + h +
+    'px;float:left;text-align:center">';
+
+  for (let i = 0; i < nTicks; i++) {
+
+    //Draw a tick
+    st += '<div  class="tick" style="width:' + w / 3 + "px" + 
+      ';height:' + SIZE_OF_TICKS +
+      'px;float:right;background-color:black">' +
+      '</div>';
+    //Write the value of the tick in a separate divider.
+    st += '<div  class="tick" style="width:' + 2 * w / 3 + "px" +
+      ';height:' + SIZE_OF_TICKS +
+      'px;float:right">' +
+      ((nTicks - i) * yMax / nTicks).toFixed(1) + '</div>';
+
+    //Add blank space inbetween the ticks
+    st += '<div  class="blankSpace" style="width:' + w + "px" +
+      ';height:' + sizeOfBlanks +
+      'px;float:left;text-align:center"></div>';
+
+  }
+  return st + '</div>'; //Closes the main divider that conatins the ticks and returns it
+}
+
+function drawDiv(h, w, text, className) {
+  //creates and adds a divider with the height + width in element e.
+  let st = '<div id="title" class="' +
+    className +
+    '" style="width:' + w + "px" +
+    ';height:' + h +
+    'px;float:left;text-align:center">' + text + '</div>';
+  return st;
+}
+
+
+function drawXLabels(data, h, w, bufferWidth) { //The labels that show up below the x-axis
+  let nBars = Object.keys(data).length; //# of labels of data we have to add
+  let st = "";
+
+  //Between each label there is a 'blank space' of width bufferWidth
+  //So the width of the labels is the total width minus the space taken by the blank bars
+  //If there are nBars labels, then there are nbars+1 label buffers (one at beginning, one at the end)
+  //So the width of the label is the remaining space divided by the number of bars
+  let barWidth = (w - bufferWidth * (nBars + 1)) / (nBars);
+
+  //This adds a space between the y-axis and the first bar.
+  let initialSpace = parseFloat(bufferWidth) + 60.;
+  st += '<div style="width:' + initialSpace + "px" + ';height:' + h + 'px;float:left"></div>';
+
+  for (barVals in data) {
+    //Creates a label divider. The class xlabel can be manipulated as needed
+    st += '<div class = "xlabel" style="width:' + barWidth + "px" + ';height:' + h + 'px"> '
+      + barVals + '</div>';
+
+    //This adds a space after each bar
+    st += '<div style="width:' + bufferWidth + "px" + ';height:' + h + 'px;float:left"></div>';
+  }
+  return st;
+}
+
+
+//Belwo are additional helper functions
+
+
+function arrayify(d) {
+  //Changes any values that are not an array into an array of length one.
+  //This is in order to ensure the rest of the functions can assume an array is given.
+  for (key of Object.keys(d)) {
+    let val = d[key];
+    if (typeof val != "object") {
+      d[key] = [val];
+    }
+  }
+  return d;
+}
+
+
+
 function randomHex() {
 
   //Selects three random numbers btwn 0-256 and converts it to base 16. Adds a leading zero if needed to each of these numbers
@@ -246,6 +259,7 @@ function randomHex() {
     ("0" + (Math.floor(Math.random() * 256)).toString(16)).slice(-2) +
     ("0" + (Math.floor(Math.random() * 256)).toString(16)).slice(-2);
 }
+
 function findMax(d) {
   let max = 0;
   for (datum in d) {
